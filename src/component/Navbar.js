3275d386d5af0bbca1f9cfff510b1c32f4e86d39
Beam2003 <nawaporn.laoa@mail.kmutt.ac.th>
2025-10-16 10:48:31 +0700

    update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,11 +9,13 @@ export default function Navbar() {
 
   // ไอคอนแก้วน้ำ (SVG)
   const WaterGlassIcon = () => (
+    
     <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
       <path d="M15.2 3.2a.5.5 0 0 0-.6.4L13 10h6l-1.6-6.4a.5.5 0 0 0-.6-.4Z"/>
       <path d="m6 10 1.5-6.4a.5.5 0 0 1 .6-.4h8.8a.5.5 0 0 1 .6.4L18 10"/>
       <path d="M6 10h12"/>
       <path d="M6 10v10c0 .6.4 1 1 1h10c.6 0 1-.4 1-1V10"/>
+      
     </svg>
   );
 
