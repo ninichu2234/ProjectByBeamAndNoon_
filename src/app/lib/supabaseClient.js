d7cfb988b737ef3087ef9f077986fdd8921b3af3
Beam2003 <nawaporn.laoa@mail.kmutt.ac.th>
2025-10-24 00:42:57 +0700

    eiei

--- src/app/lib/supabaseClient.js
+++ src/app/lib/supabaseClient.js
@@ -4,8 +4,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 // ดึงค่า URL และ Key มาจาก Environment Variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://rcrntadwwvhyojmjrmzh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjcm50YWR3d3ZoeW9qbWpybXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNjU2MzAsImV4cCI6MjA3Mzc0MTYzMH0.sMK4cdz4iB95ZycKg3srZQZm_orBEq45az5pkObPGnA';
 
 // ‼️ แก้ไข: สร้าง client ด้วย createBrowserClient()
 // นี่คือ "เวอร์ชันที่ฉลาด" ที่สามารถอ่าน/เขียน "คุกกี้" session
