5afe56f3558aab954e211b2f11bc5263ebddb82d
Beam2003 <nawaporn.laoa@mail.kmutt.ac.th>
2025-10-24 00:15:59 +0700

    updatee

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    
     reactStrictMode: true,
     images: {
       remotePatterns: [
@@ -17,6 +18,7 @@ const nextConfig = {
         },
       ],
     },
+
   };
   
   export default nextConfig;
